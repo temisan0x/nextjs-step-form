83f9f3b7b0d8b935c1afd7010a4ffe00a5e0a07e
Heytemisan <temyicy@gmail.com>
2022-04-28 16:12:29 +0100

    chore: bug fixed 🐛

--- src/components/steps/StepOutput.tsx
+++ src/components/steps/StepOutput.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import classes from '../../../styles/index.module.css';
-import { StepProps } from './StepTwo';
-import { useSelector } from 'react-redux';
+import React from "react";
+import classes from "../../../styles/index.module.css";
+import { StepProps } from "./StepTwo";
+import { useSelector } from "react-redux";
+import Image from "next/image";
+import Check from "../../assets/check.png";
 
-const StepOutput = ({ successMessage}: StepProps) => {
+const StepOutput = ({ successMessage }: StepProps) => {
+  const state = useSelector((state) => state);
+  const stateOutput = `Form-Completed:${JSON.stringify(state, null, 2)}`;
+  console.log(stateOutput);
 
-    const state = useSelector(state => state);
-    const stateOutput = (`JSON Data Form-Completed:${JSON.stringify(state, null, 2)}`)
-    console.log(stateOutput);
-    
-    return (
-        <div>
-            <pre>{stateOutput}</pre>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className={classes.logo}>
+        <Image src={Check} alt="success" width={40} height={40} />
+      </div>
+      <div className={classes.formResult}>
+        <h4>{successMessage || "Thank you, please check your email!"}</h4>
+        <pre>{stateOutput}</pre>
+      </div>
+    </>
+  );
+};
 
-export default StepOutput
+export default StepOutput;
